57fb5b41d4c831a5b54be6391e0053a6ce1c16f6
Alexandra Shparberg <alexandras@alexandras-air.mynetworksettings.com>
2024-12-18 17:53:13 -0500

    added module

--- question7-ec2-module/main.tf
+++ question7-ec2-module/main.tf
@@ -1,14 +1,11 @@
-resource "aws_instance" "ec2_instance" {
-  count = var.instance_count
-  ami = var.ami_id
-  instance_type = var.instance_type
-  key_name = var.key_name
-  security_groups = var.security_groups #with port 22 opened inbound
-  subnet_id = var.subnet_id  
-  tags = var.tags
-}
-
-resource "aws_eip" "lb" {
-  instance = aws_instance.web.id
-  domain   = "vpc"
+module "ec2_module" {
+  source          = "/Users/alexandras/Desktop/terraform-project/modules/ec2-modules"
+  ami_id          = var.ami_id
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  security_groups = var.security_groups
+  subnet_id       = var.subnet_id
+  instance_count  = var.instance_count
+  create_eip      = var.create_eip
+  tags            = var.tags
 }
